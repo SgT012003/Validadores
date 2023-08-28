818ec3306cd36982978bee43c980e681f638be2a
Victor Didoff <82065998+SgT012003@users.noreply.github.com>
2023-08-27 23:01:30 -0300

    Add Validators To main
    Add Validators for CPF, CNPJ and Email in non-online methods!
    the main is for exemple and test it!

---
+++ Validation/Validators/Static/Method/CPF.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Validation.Validators.Static.Interface;
+
+namespace Validation.Validators.Static.Method
+{
+    public class CPF : ICPF
+    {
+        public bool Validator(string cpf)
+        {
+            cpf = new string(cpf.Where(char.IsDigit).ToArray());
+            if (cpf.Length != 11)
+            {
+                return false;
+            }
+            if (cpf.Distinct().Count() == 1)
+            {
+                return false;
+            }
+            int[] cpfDigits = cpf.Select(c => int.Parse(c.ToString())).ToArray();
+            int sum = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                sum += cpfDigits[i] * (10 - i);
+            }
+            int firstVerifierDigit = 11 - sum % 11;
+            firstVerifierDigit = firstVerifierDigit > 9 ? 0 : firstVerifierDigit;
+            sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += cpfDigits[i] * (11 - i);
+            }
+            int secondVerifierDigit = 11 - sum % 11;
+            secondVerifierDigit = secondVerifierDigit > 9 ? 0 : secondVerifierDigit;
+            if (cpfDigits[9] != firstVerifierDigit || cpfDigits[10] != secondVerifierDigit)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Validation/Validators/Task/Method/Email.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Validation.Validators.Task.Interface;
+
+namespace Validation.Validators.Task.Method
+{
+    public class Email : IEmail
+    {
+#pragma warning disable CS1998
+        public async Task<bool> Validator(string email)
+        {
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            return Regex.IsMatch(email, pattern);
+#pragma warning restore CS1998
+        }
+    }
+}
